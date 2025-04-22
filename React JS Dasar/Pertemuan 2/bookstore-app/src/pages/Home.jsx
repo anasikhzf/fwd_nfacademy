d8d3a022c112150274b478d2bd3e8eaaff453837
anasikhzf <anasikhzf@gmail.com>
2025-04-23 00:23:48 +0700

    anasikhzf

--- React JS Dasar/Pertemuan 2/bookstore-app/src/pages/Home.jsx
+++ React JS Dasar/Pertemuan 2/bookstore-app/src/pages/Home.jsx
@@ -1,11 +1,18 @@
+import React from 'react';
+import Hero from './Hero';  // Mengimpor Hero
+import Book from './Book';  // Mengimpor Book
+import Team from './Team';  // Mengimpor Team
+import Contact from './Contact';  // Mengimpor Contact
+
 function Home() {
-    return (
-      <div className="container mt-5">
-        <Hero />
-        <Book />
-        <Team />
-        <Contact />
-      </div>
-    );
-  }
-  export default Home;
+  return (
+    <div className="container mt-5">
+      <Hero />
+      <Book />
+      <Team />
+      <Contact />
+    </div>
+  );
+}
+
+export default Home;
